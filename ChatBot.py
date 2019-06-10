e32b1173f52388eb5ca69528bad7c69d04969228
ZiadMaged <Ziad_7@live.com>
2019-06-11 00:37:44 +0200

    Updating Dataset and Chatbot.py

--- ChatBot.py
+++ ChatBot.py
@@ -2,21 +2,21 @@ from past.builtins import raw_input
 
 from User import *
 from NLU_Engine import NLU
-from googletrans import Translator
+# from googletrans import Translator
 #### Feedback
 #### Logs
 #### Another Question while asking for entity value
 #### Change Yaml entities values to LowerCase
 #### Response
-def Translate_Text (question):
-    translator = Translator()
-    translated = translator.translate(question )
-    print("Source Language : " + translated.src)
-    print("Destination Language : " + translated.dest)
-    print("Origin Text : " + translated.origin)
-    print("Translated Text : " + translated.text)
-    return translated.text , translated.src
-    # print("Pronunciation : " + str(translated.pronunciation))
+# def Translate_Text (question):
+#     translator = Translator()
+#     translated = translator.translate(question )
+#     print("Source Language : " + translated.src)
+#     print("Destination Language : " + translated.dest)
+#     print("Origin Text : " + translated.origin)
+#     print("Translated Text : " + translated.text)
+#     return translated.text , translated.src
+#     # print("Pronunciation : " + str(translated.pronunciation))
 
 if __name__ == "__main__":
 
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         # print(eda(question))
 
         # nlu.setQuery(Translate_Text(question))
-        nlu.setQuery((question))
+        nlu.setQuery(question)
 
         nlu.answer()
 
